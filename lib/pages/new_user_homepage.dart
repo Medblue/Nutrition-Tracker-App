512bbb0ff6850bc51c80c5216f1b4653fc1ab6f4
madhurya-ops <madhuryamishra@gmail.com>
2024-07-03 14:22:05 +0530

    Inserted Graph

--- lib/pages/new_user_homepage.dart
+++ lib/pages/new_user_homepage.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
-//import 'package:nutritrackerapp/pages/user_info_or_user_home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:community_charts_flutter/community_charts_flutter.dart' as charts;
 
 class NewUserHomepage extends StatelessWidget {
   const NewUserHomepage({super.key});
 
+  // Sign out method
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: AppBar(
+          automaticallyImplyLeading: false, // Removes back button
+          backgroundColor: Color(0xFF40E0D0),
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            'Patient Info',
+            style: TextStyle(
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+              letterSpacing: -0.02,
+              color: Colors.black,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout, color: Colors.black),
+              onPressed: signUserOut,
+            ),
+          ],
+        ),
+      ),
       body: Container(
         width: size.width,
         height: size.height,
@@ -24,69 +56,124 @@ class NewUserHomepage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.only(left: 180, top: 26, right: 10),
-                height: 68,
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      width: 0.5,
-                    ),
+              const SizedBox(height: 50),
+              // Adding the chart here
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  height: 300,
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.black,
                   ),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    
-                    const Text(
-                      'Patient Info',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                        letterSpacing: -0.02,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(width: 48), // Placeholder to balance the row
-                  ],
+                  child: SimpleTimeSeriesChart.withSampleData(),
                 ),
               ),
-              const SizedBox(height: 200,),
+              const SizedBox(height: 60),
               const Align(
                 alignment: Alignment.topLeft,
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  padding: EdgeInsets.symmetric(horizontal: 25),
                   child: Text("Notes/Reminder"),
                 ),
-                ),
+              ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Container(
                   height: 200.0,
                   child: TextField(
                     maxLines: null,
                     expands: true,
-                     decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            filled: true,
-                            enabledBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                            ),
-                            focusedBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                            ),
-                            hintText: "Observation: ",
-                            hintStyle: TextStyle(color: Colors.grey[500])
-                          ),
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      hintText: "Observation: ",
+                      hintStyle: TextStyle(color: Colors.grey[500]),
+                    ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+/// Timeseries chart example
+class SimpleTimeSeriesChart extends StatelessWidget {
+  final List<charts.Series<TimeSeriesSales, DateTime>> seriesList;
+  final bool animate;
+
+  SimpleTimeSeriesChart(this.seriesList, {this.animate = false});
+
+  /// Creates a [TimeSeriesChart] with sample data and no transition.
+  factory SimpleTimeSeriesChart.withSampleData() {
+    return SimpleTimeSeriesChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return charts.TimeSeriesChart(
+      seriesList,
+      animate: animate,
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
+      primaryMeasureAxis: const charts.NumericAxisSpec(
+        renderSpec: charts.GridlineRendererSpec(
+          labelStyle: charts.TextStyleSpec(
+            color: charts.MaterialPalette.white,
+          ),
+        ),
+      ),
+      domainAxis: const charts.DateTimeAxisSpec(
+        renderSpec: charts.SmallTickRendererSpec(
+          labelStyle: charts.TextStyleSpec(
+            color: charts.MaterialPalette.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    final data = [
+      TimeSeriesSales(DateTime(2017, 9, 19), 5),
+      TimeSeriesSales(DateTime(2017, 9, 26), 25),
+      TimeSeriesSales(DateTime(2017, 10, 3), 100),
+      TimeSeriesSales(DateTime(2017, 10, 10), 75),
+    ];
+
+    return [
+      charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.time,
+        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime time;
+  final int sales;
+
+  TimeSeriesSales(this.time, this.sales);
 }
